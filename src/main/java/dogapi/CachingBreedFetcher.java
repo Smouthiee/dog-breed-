4bdb02bb28cf6b5a8877a2a42c11a957ad40028a
Smouthiee <frankjiasheng.li@mail.utoronto.ca>
2025-10-20 16:28:30 -0400

    Task 1-4

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -1,5 +1,6 @@
 package dogapi;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -13,16 +14,27 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
     private int callsMade = 0;
+    private final BreedFetcher fetcher;
+    private final Map<String, List<String>> cache = new HashMap<>();
     public CachingBreedFetcher(BreedFetcher fetcher) {
-
+        this.fetcher = fetcher;
     }
 
     @Override
     public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        String key = breed.toLowerCase();
+        if (cache.containsKey(key)) {
+          return cache.get(key);
+        }
+        try {
+            List<String> subBreeds = fetcher.getSubBreeds(breed);
+            callsMade++;
+            cache.put(key, subBreeds);
+            return subBreeds;
+        } catch (BreedNotFoundException e) {
+            throw e;
+        }
     }
 
     public int getCallsMade() {
